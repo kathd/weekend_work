df4382e962e053de74b5aa98c3cbff040087bbd3
kathd <kathleensdomingo@gmail.com>
2017-08-24 21:53:30 -0700

    found the longest word

--- algo/find_longest_word.rb
+++ algo/find_longest_word.rb
@@ -1,6 +1,18 @@
 # Complete the method called find_longest_word, which will accept a string as a parameter (usually a sentence), and return another string that will be the longest word in that sentence.
 
 def find_longest_word(sentence)
+  words_arr = sentence.split(" ")
+  idx = 1
+  longest_word = words_arr[0]
+
+  while idx < words_arr.length
+    if words_arr[idx].length > longest_word.length
+      longest_word = words_arr[idx]
+    end
+    idx += 1
+  end
+
+  longest_word
 
 end
 
@@ -11,7 +23,7 @@ puts
 result = find_longest_word("What is the longest word in this phrase?")
 
 puts "Your method returned:"
-puts result
+p result
 puts
 
 if result == "longest"
